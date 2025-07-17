4b51a381bc6a0a9daf9fa3f3aef8c6c9521d7541
Rajput diksha singh <dikshatouchwood@gmail.com>
2025-07-17 10:58:57 +0530

    set support chat

--- lib/my.screens/support/support_screen.dart
+++ lib/my.screens/support/support_screen.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:forex_mountain/my.screens/support/create_support_ticiket.dart';
 import 'package:iconsax/iconsax.dart';
 
-import '../../utils/picture_utils.dart'; // This should contain your image provider
+import '../../utils/picture_utils.dart';
+import '../../utils/text.dart'; // This should contain your image provider
 
 class SupportScreen extends StatefulWidget {
   const SupportScreen({super.key});
@@ -15,88 +16,61 @@ class SupportScreen extends StatefulWidget {
 class _SupportScreenState extends State<SupportScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBody: true,
-      drawer: Drawer(
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: userAppBgImageProvider(context),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: ListView(
-            padding: const EdgeInsets.all(16),
-            children: const [
-              DrawerHeader(
-                child: Text("Menu", style: TextStyle(color: Colors.white, fontSize: 24)),
-              ),
-              ListTile(
-                leading: Icon(Iconsax.support, color: Colors.white),
-                title: Text("Support", style: TextStyle(color: Colors.white)),
-              ),
-              // Add more drawer items here
-            ],
-          ),
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: userAppBgImageProvider(context),
+          fit: BoxFit.cover,
         ),
       ),
-      appBar: AppBar(
-        title: const Text('Support'),
-        backgroundColor: Colors.black87,
-        centerTitle: true,
-        elevation: 0,
-        leading: Builder(
-          builder: (context) => IconButton(
-            icon: const Icon(Icons.menu, color: Colors.white),
-            onPressed: () => Scaffold.of(context).openDrawer(),
+      child: Scaffold(
+        extendBody: true,
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: bodyLargeText('Support', context, fontSize: 20),
+          backgroundColor: Colors.black,
+          elevation: 0,
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const CreateSupportTicket()),
+            );
+          },
+          backgroundColor: Colors.lightBlueAccent,
+          icon: const Icon(Iconsax.add),
+          label: const Text("Open Ticket"),
+        ),
+        body: const SafeArea(
+          child: Padding(
+            padding:
+            EdgeInsets.symmetric(horizontal: 16.0, vertical: 12),
+            child: Column(
+              children: [
+                // Glass card with status counts
+                GlassCard(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      _StatusCount(label: "Open", count: "1", color: Colors.red),
+                      _StatusCount(
+                          label: "In Progress", count: "0", color: Colors.green),
+                      _StatusCount(
+                          label: "Answered", count: "0", color: Colors.blue),
+                      _StatusCount(
+                          label: "On Hold", count: "0", color: Colors.orange),
+                      _StatusCount(label: "Closed", count: "0", color: Colors.grey),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-        titleTextStyle: const TextStyle(color: Colors.white, fontSize: 20),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const CreateSupportTicket()),
-          );
-        },
-        backgroundColor: Colors.lightBlueAccent,
-        icon: const Icon(Iconsax.add),
-        label: const Text("Open Ticket"),
-      ),
-      body: Stack(
-        children: [
-          // Background image using your image provider
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: userAppBgImageProvider(context), // <-- main body background
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-
-          // Glass card with status counts
-          Positioned(
-            top: 20,
-            left: 16,
-            right: 16,
-            child: GlassCard(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: const [
-                  _StatusCount(label: "Open", count: "1", color: Colors.red),
-                  _StatusCount(label: "In Progress", count: "0", color: Colors.green),
-                  _StatusCount(label: "Answered", count: "0", color: Colors.blue),
-                  _StatusCount(label: "On Hold", count: "0", color: Colors.orange),
-                  _StatusCount(label: "Closed", count: "0", color: Colors.grey),
-                ],
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
